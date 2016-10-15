ed9d7c4818dd4c4e33a6aa4ef2642c0615b24bcd
Chris Phillips <phillips.1430@buckeyemail.osu.edu>
2016-10-15 02:45:25 -0400

    text switch button #BestCodeEver

--- CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/TutorialActivity.java
+++ CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/TutorialActivity.java
@@ -1,26 +1,44 @@
 package com.example.fo893113.choices2016fo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TextSwitcher;
 
 public class TutorialActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // Array of String to Show In Text Switcher
+    String textToShow[]={"Message 1","Message 2 ","Message 3","Message 4"};
+    int msgCount = textToShow.length;
+    int currentIndex = 0;
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.content_tutorial);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutorial);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button nextBtn = (Button)findViewById(R.id.nextButton);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClick();
+            }
 
-
-
-
-        //You can click the back button
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            private void onClick(){
+                if(currentIndex < msgCount) {
+                    TextView textView=(TextView)findViewById(R.id.textView);;
+                    textView.setText(textToShow[currentIndex]);
+                    currentIndex++;
+                }
+                else{
+                    //go to next activity
+                }
+            }
+        });
     }
+
+
 
 }
