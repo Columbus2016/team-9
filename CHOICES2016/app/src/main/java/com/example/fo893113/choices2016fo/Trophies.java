42b85b140e7ddd71411fd9fcd487cbb7747ea7d7
diegoalva96 <diegoe.alvarez14@gmail.com>
2016-10-15 09:23:04 -0400

    #EpicFail fixes crashing bug when you don't continue

--- CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/Trophies.java
+++ CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/Trophies.java
@@ -7,23 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class badges extends AppCompatActivity {
+public class Trophies extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_badges);
+        setContentView(R.layout.activity_trophies);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
