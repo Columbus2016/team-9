793055e95bb7dfc30175985898c685072299ae4c
Chris Phillips <phillips.1430@buckeyemail.osu.edu>
2016-10-15 05:26:44 -0400

    #ImWithCode scenario 1

--- CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/scenario1.java
+++ CHOICES2016/app/src/main/java/com/example/fo893113/choices2016fo/scenario1.java
@@ -1,30 +1,45 @@
 package com.example.fo893113.choices2016fo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class scenario1 extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // Array of String to Show In Text Switcher
+    String dialogueTextToShow[]={"Dialogue 1 ","Dialogue 2"};
+    int dialogueCurrentIndex = 0;
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.content_scenario1);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scenario1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button optBtnOne = (Button)findViewById(R.id.option1);
+        Button optBtnTwo = (Button)findViewById(R.id.option2);
+        final TextView result = (TextView) findViewById(R.id.Result);
+        optBtnOne.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                onClickBtnOne();
+            }
+
+            private void onClickBtnOne(){
+                result.setText(dialogueTextToShow[0]);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        optBtnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBtnTwo();
+            }
+
+            private void onClickBtnTwo(){
+                result.setText(dialogueTextToShow[1]);
+            }
+        });
+    }
 }
